81c68df2e803572718877c8db124735612343b7e
ClaudineRas02 <hanitraclaudine@gmail.com>
2026-09-10 14:57:02 +0300

    fix: api config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
+console.log("API Config:", import.meta.env.VITE_API_HOSTNAME);
+
 const APP_CONFIG = {
   API_CONFIG: {
     hostname: import.meta.env.VITE_API_HOSTNAME,
-    port: import.meta.env.VITE_API_PORT
+    port: import.meta.env.VITE_API_PORT,
   },
 };
 
